46021eff4e32b662ba508262a96e18fb1132ecee
nhintt <nhintt2411@gmail.com>
2020-10-08 14:26:10 +0700

    push project

---
+++ Test Suites/Suite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>76580326-52d7-4ff5-9fec-be6b43826df4</testSuiteGuid>
+   <testCaseLink>
+      <guid>b2233849-6c13-4952-ae95-8d851e88a56c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_001_Profile</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
